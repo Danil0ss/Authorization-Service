03e5f35ac330ac1b701085a360ea31ef96c6cba1
Danil0ss <2005prudnikov@gmail.com>
2026-07-11 00:07:15 +0300

    schema and migration creation

---
+++ src/main/java/com/example/authService/Entity/User.java
@@ -0,0 +1,42 @@
+package com.example.authService.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+@Id
+@GeneratedValue(strategy =GenerationType.UUID)
+private UUID id;
+
+@Column(nullable = false,unique = true,length = 100)
+private String email;
+
+@Column(nullable = false)
+private String password;
+
+@Column(nullable = false)
+private Boolean isActive;
+
+@ElementCollection(fetch = FetchType.EAGER)
+@CollectionTable(
+        name ="roles",
+        joinColumns = @JoinColumn(name = "user_id")
+)
+@Column(name = "role")
+@Enumerated(EnumType.STRING)
+private Set<Role> roles=new HashSet<>();
+}
