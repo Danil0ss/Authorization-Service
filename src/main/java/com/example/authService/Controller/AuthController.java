0c83e38050dbd8d81fcae95991c76096ac673621
Danil0ss <2005prudnikov@gmail.com>
2026-07-14 18:58:55 +0300

    add validation

--- src/main/java/com/example/authService/Controller/AuthController.java
+++ src/main/java/com/example/authService/Controller/AuthController.java
@@ -4,6 +4,7 @@ import com.example.authService.Dto.JwtResponse;
 import com.example.authService.Dto.LoginDto;
 import com.example.authService.Dto.RegisterDto;
 import com.example.authService.Service.AuthService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,13 +20,13 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterDto registerDto){
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterDto registerDto){
         authService.register(registerDto);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JwtResponse> login(@RequestBody LoginDto loginDto) {
+    public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginDto loginDto) {
         String token = authService.login(loginDto);
         return ResponseEntity.ok(new JwtResponse(token));
     }
--- src/main/java/com/example/authService/Dto/RegisterDto.java
+++ src/main/java/com/example/authService/Dto/RegisterDto.java
@@ -1,7 +1,15 @@
 package com.example.authService.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record RegisterDto(
+        @NotBlank(message = "Email can't be empty")
+        @Email
         String email,
+        @NotBlank(message = "Password can't be empty")
+        @Size(min = 6,message = "Password must contain at least 6 symbols")
         String password
 ) {
 }
